9605dffe286ee2d92409aad25e45669710443742
glooooh <glorinhamsouza@gmail.com>
2021-02-22 14:48:05 -0300

    Primeiro teste

---
+++ application/views/novo_gasto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>sla</h1>
+    <form action="">
+        <label for="data">Data: <input type="date" name="data"></label><br>
+        <label for="descricao">Descrição: <input type="text" name="descricao"></label><br>
+        <label for="valor">Valor: <input type="number" name="valor"></label><br>
+        <button type="button">Enviar</button>
+        <button type="reset">Limpar</button>
+    </form>
+</body>
+</html>
